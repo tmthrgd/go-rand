9345b8b91204d798ad9321499b9fe38c37c5b8be
Tom Thorogood <me+github@tomthorogood.co.uk>
2018-01-06 23:45:57 +1030

    Add command line go-rand tool

---
+++ cmd/go-rand/rand.go
@@ -0,0 +1,39 @@
+// Copyright 2018 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"io"
+	"os"
+
+	"github.com/tmthrgd/go-rand"
+)
+
+func main() {
+	var n uint
+	flag.UintVar(&n, "c", 0, "the number of bytes to read")
+
+	flag.Parse()
+
+	var limit bool
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == "c" {
+			limit = true
+		}
+	})
+
+	var err error
+	if limit {
+		_, err = io.CopyN(os.Stdout, rand.Reader, int64(n))
+	} else {
+		_, err = io.Copy(os.Stdout, rand.Reader)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+}
